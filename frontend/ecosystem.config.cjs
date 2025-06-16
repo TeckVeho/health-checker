d5efe0d4dd2182bee2c5500268ed3d0bb943eedd
KidoVeho <136041145+KidoVeho@users.noreply.github.com>
2025-06-16 18:38:25 +0900

    update ecosystem.config.cjs

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -1,17 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: 'frontend-prod',
-        script: 'node_modules/.bin/nuxt',
-        args: 'start',
-        cwd: './frontend',
-        interpreter: '/home/ec2-user/.nvm/versions/node/v22.15.0/bin/node',
-        exec_mode: 'fork',
-        watch: false,
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'frontend-prod',
+      script: '.output/server/index.mjs',
+      cwd: './frontend',
+      interpreter: '/home/ec2-user/.nvm/versions/node/v22.15.0/bin/node',
+      exec_mode: 'fork',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
-  
+    },
+  ],
+};
