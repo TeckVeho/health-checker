8af9375ff85f2f6914629f7f482e73742661439d
KidoVeho <136041145+KidoVeho@users.noreply.github.com>
2025-06-17 12:20:08 +0900

    use .env for deploy

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -1,16 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   apps: [
     {
       name: 'frontend-prod',
       script: '.output/server/index.mjs',
       cwd: './',
-      interpreter: '/home/ec2-user/.nvm/versions/node/v22.15.0/bin/node',
+      interpreter: process.env.NODE_INTERPRETER || 'node',
       exec_mode: 'fork',
       watch: false,
       env: {
         NODE_ENV: 'production',
-        HOST: '0.0.0.0',
-        PORT: 5001,
+        HOST: process.env.NITRO_HOST,
+        PORT: process.env.NITRO_PORT,
+        API_BASE_URL: process.env.API_BASE_URL,
       },
     },
   ],
